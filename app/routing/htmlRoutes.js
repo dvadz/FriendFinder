f5101bdc7f15203d9c91e485138be0430be60b6f
Daniel Vadil <dvadz@yahoo.com>
2019-05-04 02:23:52 -0400

    cleaned up the console.logs

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,18 +3,19 @@ var express = require('express');
 var htmlrouter = express.Router();
 
 htmlrouter.get('/', function(req, res){
-    console.log(path.join(__dirname, '../..', 'app/public/home.html'));
+    console.log("Serving home page");
     res.sendFile(path.join(__dirname, '../..', 'app/public/home.html'));
 });
 
 // SURVEY
 htmlrouter.get('/survey', function(req, res){
-    console.log(path.join(__dirname, '../..', 'app/public/survey.html'));
+    console.log("Serving survey");
     res.sendFile(path.join(__dirname, '../..', 'app/public/survey.html'));
 });
 
 //catch-all
 htmlrouter.get("*", function(req, res){
+    console.log("Catch-all");
     res.redirect('/');
 });
 
