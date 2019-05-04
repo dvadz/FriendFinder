8602290ead52e6abf7f6204cbb14f703c8e4fabe
Daniel Vadil <dvadz@yahoo.com>
2019-05-03 21:06:23 -0400

    moved the html routes from the server.js to the htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,21 @@
-// TODO: * A GET Route to `/survey` which should display the survey page.
-// TODO: * A default, catch-all route that leads to `home.html` which displays the home page.
+var path = require('path');
+var express = require('express');
+var htmlrouter = express.Router();
+
+htmlrouter.get('/', function(req, res){
+    console.log(path.join(__dirname, '../..', 'app/public/home.html'));
+    res.sendFile(path.join(__dirname, '../..', 'app/public/home.html'));
+});
+
+// SURVEY
+htmlrouter.get('/survey', function(req, res){
+    console.log(path.join(__dirname, '../..', 'app/public/survey.html'));
+    res.sendFile(path.join(__dirname, '../..', 'app/public/survey.html'));
+});
+
+//catch-all
+htmlrouter.get("*", function(req, res){
+    res.redirect('/');
+});
+
+module.exports = htmlrouter;
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 var express = require("express");
 var path = require("path");
-
+var fs = require("fs");
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("WELCOME TO FRIENDFINDER");
-});
+
+var router = require('./app/routing/htmlRoutes.js');
+app.use(router);
 
 app.listen(PORT, function(){
     console.log(`Friendfinder is running on PORT ${PORT}`);
 });
+
+// =========================================================
+
+function serveHTML(file, res){
+    fs.readFile(path.join(__dirname, file), function(err, data){
+        if(err) throw err;
+
+        res.end(data);
+        console.log(__dirname + file);
+    });
+}
+
 
