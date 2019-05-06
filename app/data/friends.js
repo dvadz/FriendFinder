826e4ed2e8c97aed84e459cbf8baf1262e6aace8
Daniel Vadil <dvadz@yahoo.com>
2019-05-06 02:10:24 -0400

    added a constructor

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,25 @@
 // TODO: 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-var friends = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-    }
-];
+function Friends() {
+    // TODO: create some 
+    this.friends = [
+        {
+            "name":"Ahmed",
+            "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        },
+        {
+            "name":"Barock",
+            "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores":[1, 3, 2, 3, 5, 5, 4, 5, 3, 2]
+        }
+    ];
+}
 
-module.exports = friends;
+Friends.prototype.findAMatch = function() {
+    console.log("Running proprietary algorthm to calculate the perfect match");
+
+    //get the last 'friend'
+}
+
+module.exports = Friends;
